49ffef9ea7612bca214a90d5cbca4ebc1219dbed
Danny Burevski <dannyburevski@gmail.com>
2022-05-03 15:55:27 +0300

    pre-project-JDBC-1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("David", "Beckham", (byte) 47);
+        us.saveUser("Roberto", "Carlos", (byte) 49);
+        us.saveUser("Edgar", "Davids", (byte) 49);
+        us.saveUser("Wesley", "Sneijder", (byte) 37);
+
+        us.removeUserById(2);
+        us.getAllUsers();
+        us.cleanUsersTable();
+        us.dropUsersTable();
+        Util.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,41 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3307/mykata";
+    private static final String USER = "root";
+    private static final String PASSWORD = "0808Ufkjpfkby";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection () {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Connection not established");
+        }
+        return connection;
+    }
+
+    public static void closeConnection() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
